3ed48d5852ce8b49f5ccc24bee1a5a063fd63ba9
weslleysr85 <weslleysr85@gmail.com>
2022-08-04 14:53:15 +0000

    css edit flexbox

--- peace-in-space/public/gameRender.js
+++ peace-in-space/public/gameRender.js
@@ -4,7 +4,18 @@ export default function createGameRender (game)
     const screen = document.getElementById("screen");
     const context = screen.getContext('2d');
     requestAnimationFrame(renderScreen); //executa a função renderScreen() que chama ela mesma a cada frame
-   
+
+    // imagens renderizadas 
+    const playerImg = new Image();        
+    const outerPlayerImg = new Image();
+    const itemImg = new Image();
+    playerImg.src = "img/nave.png";
+    outerPlayerImg.src = "img/nave-parceira.png";
+    itemImg.src = "img/item.png";
+
+    
+    const highScore = document.getElementById("highScore");; //HUD
+
     function renderScreen () 
     {
        
@@ -18,7 +29,7 @@ export default function createGameRender (game)
             context.fillRect(rect.x*pixelSize, rect.y*pixelSize, pixelSize, pixelSize);                    
         }
 
-        function drawImage (img){            
+        function drawImg (img){            
             context.drawImage(img.myImage, img.x*pixelSize, img.y*pixelSize, pixelSize, pixelSize);
         }
        
@@ -26,20 +37,20 @@ export default function createGameRender (game)
         for(const pid in game.state.players){
             const player = game.state.players[pid];
            // drawRect({...player, color:pid===game.current?'yellow':'black'})
-            let img  = document.getElementById("playerImage");
-            let img2 = document.getElementById("outerPlayerImage");
-            drawImage ({...player, myImage : pid===game.current?img:img2}  )
+            //let img  = document.getElementById("playerImage");
+            //let img2 = document.getElementById("outerPlayerImage");            
+            drawImg ({...player, myImage : pid===game.current?playerImg:outerPlayerImg}  );
+            if (player.points > highScore.innerHTML) { highScore.innerText = player.points};//HUD
         }
 
         for(const fid in game.state.fruits){
             const player = game.state.fruits[fid];
             //drawRect({...player, color:'green'}) 
-            let img = document.getElementById("itemImage");
-            drawImage ({...player, myImage : img}  ) 
-
-
+           /* let img = document.getElementById("itemImage");*/
+            drawImg ({...player, myImage : itemImg}  );
         }
 
+        //HUD
         const playerPoints = document.getElementById("points");
         const currentPlayer = game.state.players[game.current];
         if(currentPlayer) playerPoints.innerText = currentPlayer.points;
